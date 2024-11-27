2af3eb47570c8c4c3282cf1d254727946fff239a
Martin Vanni <jm.vanni@hotmail.com>
2024-11-27 23:26:47 +0900

    new table style

--- src/components/Flashcard.jsx
+++ src/components/Flashcard.jsx
@@ -36,44 +36,106 @@ const Flashcard = ({word}) => {
         </>
       }
       backContent={
-        <div>
+        <>
           <h2 className="card-title">
             <span className={word.type}></span>
             {word.long_form || word.adjective}
           </h2>
+          {/* <p className="card-subtitles">Short Forms</p> */}
           {word.forms && (
             <>
             <table>
+              <thead className={word.type}>
+                <tr>
+                  <th scope="col">Short Forms</th>
+                </tr>
+              </thead>
               <tr>
-                <td scope="row">{word.forms.past_form}</td>
-                <td>{word.plain_form}</td>
+                <td scope="row">
+                  <span>3</span>
+                  {word.forms.past_form}
+                </td>
+                <td>
+                  <span>1</span>
+                  <strong>{word.plain_form}</strong>
+                </td>
               </tr>
               <tr>
-                <td scope="row">{word.forms.past_negative_form}</td>
-                <td>{word.forms.nai_form}</td>
+                <td scope="row">
+                  <span>4</span>
+                  {word.forms.past_negative_form}
+                </td>
+                <td>
+                  <span>2</span>
+                  {word.forms.nai_form}</td>
               </tr>
               <tr>
-                <td scope="row">{word.forms.volitional_form}</td>
+                <td scope="row">
+                  <span>5</span>
+                  {word.forms.volitional_form}
+                </td>
               </tr>
+              <thead className={word.type}>
+                <tr>
+                  <th scope="col">Conditionals</th>
+                </tr>
+              </thead>
               <tr>
-                <td scope="row">{word.forms.nakereba_form}</td>
-                <td>{word.forms.eba_form}</td>
+                <td scope="row">
+                  <span>7</span>
+                  {word.forms.nakereba_form}
+                </td>
+                <td>
+                  <span>6</span>
+                  {word.forms.eba_form}
+                </td>
+              </tr>
+              <thead className={word.type}>
+                <tr>
+                  <th scope="col">Te / De</th>
+                </tr>
+              </thead>
+              <tr>
+                <td>
+                  <span>9</span>
+                  {word.forms.de_form}
+                </td>
+                <td scope="row">
+                  <span>8</span>
+                  {word.forms.te_form}
+                </td>
+              </tr>
+              {/* <thead className={word.type}>
+                <tr>
+                  <th scope="col">Tai</th>
+                </tr>
+              </thead>
+              <tr>
+                <td scope="row">
+                  <span>8</span>
+                  {word.forms.takunai_form}
+                </td>
+                <td scope="row">
+                  <span>10</span>
+                  {word.forms.tai_form}
+                </td>
+                
+              </tr> */}
+              <thead className={word.type}>
+                <tr>
+                  <th scope="col">Potential</th>
+                </tr>
+              </thead>
+              <tr>
+                <td scope="row">
+                  <span>10</span>
+                  {word.forms.potential_form}
+                </td>
               </tr>
             </table>
-            <ul className="card-list">
-              <li>
-                Te: <strong>{word.forms.te_form}</strong>
-              </li>
-              <li>
-                De: <strong>{word.forms.de_form}</strong>
-              </li>
-              <li>
-                Potential: <strong>{word.forms.potential_form}</strong>
-              </li>
-            </ul>
             </>
           )}
-        </div>
+        </>
       }
     />
   );
